9ea70f5b8d9b2d2fe7d59b13b75f1bf55468fed1
Vlad <miroshnichenko.vladis@gmail.com>
2022-03-23 22:31:57 +0100

    первая практика

--- js/js.js
+++ js/js.js
@@ -1,8 +1,22 @@
 
 'use strict';
 
+const numberOfFilms = +prompt('сколько фильмов Вы уже посмотрели?', '');
 
-console.log( 2 && 5);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = +prompt('Какая его оценка?'),
+      c = prompt('Один из последних просмотренных фильмов?'),
+      d = +prompt('Какая его оценка?');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
+console.log(personalMovieDB);
